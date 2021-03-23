5f75d225fbd43772f3d85a5b55f02e18e2e71051
SkageLysgaard <31646924+SkageLysgaard@users.noreply.github.com>
2021-03-23 21:46:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,67 +1,20 @@
 import threading
-#import socket  the old way
-from socket import create_server
+import socket 
 
 
-#host = "localhost" # localhost
-#port = 5550
+host = "localhost" 
+port = 5550
+server.bind((host, port))
 
-# Starting Server
-server = create_server(("localhost", 5550))
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.listen()
+print("host")
+print("Waiting for ant incomming connections...")
+conn, addr = s.accept()
+print(addr, "Has connected to the server")
 
-#the old way
-#server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server.listen()
-
-# Lists For Clients and Their Nicknames
-clients = []
-nicknames = []
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-
-# Handling Messages From Clients
-def handle(client):
-    while True:
-        try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
-            break
-
-# Receiving / Listening Function
-def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
-
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
-
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
-print("Server is listening... ")
-
-receive()
-
+filename = input(str("please enter the filename of the file: "))
+file = open(filename , "rb")
+file_data = file.read(1024)
+conn.send(file_data)
+print("Data has been sendt successfully!")
